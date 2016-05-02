eb2d4dd2f2b9bcb2f50b979d59aaf498d29c6659
priyanka353 <priyanka353rocks@gmail.com>
2016-05-02 16:44:56 +0530

    for execute syntax

--- tsdb/recipes/oracle.rb
+++ tsdb/recipes/oracle.rb
@@ -17,7 +17,7 @@ when 'debian', 'ubuntu'
          command "add-apt-repository ppa:webupd8team/java;apt-get update"
      end
   execute "install oracle" do
-        command   "echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections;apt-get -y install oracle-java8-installer"
+        command   "echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 select true' | sudo debconf-set-selections;apt-get -y install oracle-java8-installer"
       end
 end
 
